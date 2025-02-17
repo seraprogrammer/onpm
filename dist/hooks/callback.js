78434dcdf1014f8f42493b6ed3ebfa01547a4c32
Sera Programmer <73139993+seraprogrammer@users.noreply.github.com>
2025-02-17 13:23:27 +0600

    Delete dist/hooks directory

--- dist/hooks/callback.js
+++
@@ -1,6 +0,0 @@
-import { createMemoHook } from "./memo.js";
-export function createCallbackHook(o) {
-  return function (e, r) {
-    return o.$memo(() => e, r);
-  };
-}
--- dist/hooks/ref.js
+++
@@ -1,7 +0,0 @@
-export function createRefHook(o) {
-  return function (n) {
-    const r = o.getCurrentInstance(),
-      t = r.currentHook++;
-    return r.hooks[t] || (r.hooks[t] = { current: n }), r.hooks[t];
-  };
-}
